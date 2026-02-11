c863e42277b53004f46ac9c802b2141d220782e5
Dev0x1 <byte0x1@gmail.com>
2026-02-11 15:23:56 -0400

    Se creo el modulo de la App.

---
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.phantomshard.michael_jose_ap2_p1.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+}
