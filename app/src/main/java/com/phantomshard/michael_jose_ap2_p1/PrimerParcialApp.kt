d967d0fb424d9b857da5f907ac97f7b10aab1e5b
Dev0x1 <byte0x1@gmail.com>
2026-02-11 15:14:48 -0400

    Se creo el PrimerParcialApp.kt

---
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/PrimerParcialApp.kt
@@ -0,0 +1,8 @@
+package com.phantomshard.michael_jose_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PrimerParcialApp : Application() {
+}
