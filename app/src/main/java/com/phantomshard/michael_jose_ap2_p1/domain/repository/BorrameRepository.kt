64b9223a7cff41a7ef52c26a77a5b72ba0f3e7d3
Dev0x1 <byte0x1@gmail.com>
2026-02-11 18:53:22 -0400

    Borrando archivo de BorrameRepository.kt

--- app/src/main/java/com/phantomshard/michael_jose_ap2_p1/domain/repository/BorrameRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.phantomshard.michael_jose_ap2_p1.domain.repository
-
-import com.phantomshard.michael_jose_ap2_p1.domain.model.Borrame
-
-interface BorrameRepository {
-    suspend fun upsert(borrame: Borrame): Int
-}
