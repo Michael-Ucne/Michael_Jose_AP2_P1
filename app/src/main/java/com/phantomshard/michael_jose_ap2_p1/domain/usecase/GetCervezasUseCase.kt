be469b265176733837ace0e8e0c5ab6bacee8e89
Dev0x1 <byte0x1@gmail.com>
2026-02-11 19:41:02 -0400

    renombra el usecase de get (a plural).

--- app/src/main/java/com/phantomshard/michael_jose_ap2_p1/domain/usecase/GetCervezasUseCase.kt
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/domain/usecase/GetCervezasUseCase.kt
@@ -5,7 +5,7 @@ import com.phantomshard.michael_jose_ap2_p1.domain.repository.CervezaRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCervezaUseCase @Inject constructor(
+class GetCervezasUseCase @Inject constructor(
     private val repository: CervezaRepository
 ) {
     operator fun invoke(): Flow<List<Cerveza>> {
