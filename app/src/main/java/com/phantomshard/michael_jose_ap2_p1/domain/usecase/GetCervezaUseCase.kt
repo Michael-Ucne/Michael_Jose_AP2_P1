19f95ee5348747af10df8389f5fa7ed988fe0ed3
Dev0x1 <byte0x1@gmail.com>
2026-02-11 19:38:42 -0400

    Agrega el usecase de get.

---
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/domain/usecase/GetCervezaUseCase.kt
@@ -0,0 +1,14 @@
+package com.phantomshard.michael_jose_ap2_p1.domain.usecase
+
+import com.phantomshard.michael_jose_ap2_p1.domain.model.Cerveza
+import com.phantomshard.michael_jose_ap2_p1.domain.repository.CervezaRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCervezaUseCase @Inject constructor(
+    private val repository: CervezaRepository
+) {
+    operator fun invoke(): Flow<List<Cerveza>> {
+        return repository.getAllCervezas()
+    }
+}
