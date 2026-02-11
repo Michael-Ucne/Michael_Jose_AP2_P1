6ac88f0679aaf8985be7f7466851770aa538dc99
Dev0x1 <byte0x1@gmail.com>
2026-02-11 18:42:43 -0400

    Creacion del Modelo de Cerveza.kt

--- app/src/main/java/com/phantomshard/michael_jose_ap2_p1/domain/model/Borrame.kt
+++
@@ -1,5 +0,0 @@
-package com.phantomshard.michael_jose_ap2_p1.domain.model
-
-data class Borrame(
-    val borrameId: Int = 0
-)
---
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/domain/model/Cerveza.kt
@@ -0,0 +1,8 @@
+package com.phantomshard.michael_jose_ap2_p1.domain.model
+
+data class Cerveza(
+    val cervezaId: Int? = null,
+    val nombre: String = "",
+    val marca: String = "",
+    val puntuacion: Int = 0
+)
