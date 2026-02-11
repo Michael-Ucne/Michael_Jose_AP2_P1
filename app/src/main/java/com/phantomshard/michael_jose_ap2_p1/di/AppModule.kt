2a1f75bfb52adf492942dccccc871b0044cf54c8
Dev0x1 <byte0x1@gmail.com>
2026-02-11 15:33:12 -0400

    Agrega hilt appmodule.

--- app/src/main/java/com/phantomshard/michael_jose_ap2_p1/di/AppModule.kt
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/di/AppModule.kt
@@ -1,10 +1,26 @@
 package com.phantomshard.michael_jose_ap2_p1.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import com.phantomshard.michael_jose_ap2_p1.data.local.database.AppDatabase
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "App.Database"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
 }
