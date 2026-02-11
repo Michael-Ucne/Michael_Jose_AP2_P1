6236695c137f88353e0da9e47dc844287eaf0d74
Dev0x1 <byte0x1@gmail.com>
2026-02-11 15:30:23 -0400

    Se crearon los Screen de List y Edit con su preview.

---
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/presentation/tareas/list/ListBorrameScreen.kt
@@ -0,0 +1,12 @@
+package com.phantomshard.michael_jose_ap2_p1.presentation.tareas.list
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun ListBorrameBodyPreview() {
+    MaterialTheme {
+    }
+}
