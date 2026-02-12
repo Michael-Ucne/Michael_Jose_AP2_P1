d9f30c3b98660ed7b0091a8d91b23467f70336e5
Dev0x1 <byte0x1@gmail.com>
2026-02-11 20:51:38 -0400

    cambio de borrame por cerveza con su route o navigate

--- app/src/main/java/com/phantomshard/michael_jose_ap2_p1/presentation/navigation/MichaelAp2NavHost.kt
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/presentation/navigation/MichaelAp2NavHost.kt
@@ -4,16 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.phantomshard.michael_jose_ap2_p1.presentation.cerveza.CervezaListScreen
+import com.phantomshard.michael_jose_ap2_p1.presentation.cerveza.CervezaEditScreen
+import androidx.navigation.toRoute
 
 @Composable
 fun MichaelAp2NavHost(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.BorrameList
+        startDestination = Screen.CervezaList
     ) {
-        composable<Screen.BorrameList> {
+        composable<Screen.CervezaList> {
+            CervezaListScreen(
+                onAddCerveza = { navController.navigate(Screen.CervezaEdit()) },
+                onEditCerveza = { id -> navController.navigate(Screen.CervezaEdit(id)) }
+            )
         }
-        composable<Screen.BorrameEdit> { backStackEntry ->
+        composable<Screen.CervezaEdit> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.CervezaEdit>()
+            CervezaEditScreen(
+                cervezaId = args.cervezaId,
+                onNavigateBack = { navController.navigateUp() }
+            )
         }
     }
 }
