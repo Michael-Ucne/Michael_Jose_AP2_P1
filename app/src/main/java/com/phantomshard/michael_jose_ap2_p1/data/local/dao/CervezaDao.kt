889f04d4acb8ca2c092607646aff21447a1e0fed
Dev0x1 <byte0x1@gmail.com>
2026-02-11 20:20:10 -0400

    cambio de nombre de funciones de impl.

--- app/src/main/java/com/phantomshard/michael_jose_ap2_p1/data/local/dao/CervezaDao.kt
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p1/data/local/dao/CervezaDao.kt
@@ -1,23 +1,20 @@
 package com.phantomshard.michael_jose_ap2_p1.data.local.dao
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Query
-import androidx.room.Upsert
+import androidx.room.*
 import com.phantomshard.michael_jose_ap2_p1.data.local.entities.CervezaEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CervezaDao {
     @Upsert
-    suspend fun upsert(cerveza: CervezaEntity)
+    suspend fun save(cerveza: CervezaEntity)
 
     @Delete
     suspend fun delete(cerveza: CervezaEntity)
 
-    @Query("SELECT * FROM Cervezas WHERE cervezaId = :id")
+    @Query("SELECT * FROM cervezas WHERE cervezaId = :id")
     suspend fun find(id: Int): CervezaEntity?
 
-    @Query("SELECT * FROM Cervezas")
+    @Query("SELECT * FROM cervezas")
     fun getAll(): Flow<List<CervezaEntity>>
 }
